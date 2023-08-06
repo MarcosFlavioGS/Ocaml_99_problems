254334433663daf94419061e5a1e98f21a284f69
Marcos Flavio <mfghost69@gmail.com>
2023-08-06 15:58:38 -0300

    Feat :Resolve second problem

--- bin/main.ml
+++ bin/main.ml
@@ -3,7 +3,15 @@ let rec last (xs: 'a list): 'a option =
   match xs with
   | [] -> None
   | [x] -> Some x
-  | _ :: rest -> last rest
+  | _ :: rest -> last rest;;
+
+(* Problem 02 *)
+let rec last_two (xs: 'a list): ('a * 'b) option =
+  match xs with
+  | [] -> None
+  | [_] -> None
+  | [x; y] -> Some (x, y)
+  | _ :: rest -> last_two rest;;
 
 let () =
   print_endline "Hello, World!"
