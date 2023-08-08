6546b4ca6ad766fdd075597c9cf9bc9c723a49bd
Marcos Flavio <mfghost69@gmail.com>
2023-08-07 23:55:41 -0300

    Feat (problem 04): Implementing tail recursion call

--- bin/main.ml
+++ bin/main.ml
@@ -9,8 +9,10 @@ let () =
   let problem_two = last_two [1; 2; 3; 4; 5] in
   let problem_three = fd_nth_element ["1"; "44"; "42"; "4"; "100"] 3 in
   let problem_four = lst_len ["1"; "2"; "3"; "4"] in
+  let problem_four_tail = lst_len_tail ["1"; "2"; "3"; "4"] in
 
   print_problem_one string_of_int problem_one;
   print_problem_two string_of_int problem_two;
   print_endline problem_three;
-  print_endline (string_of_int problem_four)
+  print_endline (string_of_int problem_four);
+  print_endline (string_of_int problem_four_tail)
--- bin/problem_four.ml
+++ bin/problem_four.ml
@@ -1,3 +1,10 @@
+let lst_len_tail (lst: 'a list): int =
+  let rec lst_len_tail' (lst: 'a list) (result: int): int =
+    match lst with
+    | [] -> result
+    | _ :: tail -> lst_len_tail' tail (result + 1)
+  in lst_len_tail' lst 0
+
 let rec lst_len (lst: 'a list) =
   match lst with
   | [] -> 0
