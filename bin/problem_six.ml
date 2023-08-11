71af16fcbc9f2a29f32c3bfe2369cdff53383904
Marcos Flavio <mfghost69@gmail.com>
2023-08-11 01:56:30 -0300

    Refact :Rename

--- bin/problem_six.ml
+++ bin/problem_six.ml
@@ -9,8 +9,8 @@ let is_palindrome (lst: 'a list): bool =
     let rec is_palindrome' (lst: 'a list) (rev: 'a list): bool =
       match (lst, rev) with
       | [], [] -> true
-      | h :: t, hr :: ht ->
-        if h = hr then is_palindrome' t ht
+      | h :: t, rh :: rt ->
+        if h = rh then is_palindrome' t rt
         else false
       | _ -> false
     in is_palindrome' lst rev
