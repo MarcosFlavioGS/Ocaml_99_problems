3ca944474c5d2dc41e9aab99b7407ed2269031ab
Marcos Flavio <mfghost69@gmail.com>
2023-08-12 11:53:39 -0300

    Feat :Print problems info.

--- bin/main.ml
+++ bin/main.ml
@@ -16,10 +16,21 @@ let () =
   let problem_five = rev_lst ["1"; "2"; "3"; "4"] in
   let problem_six = is_palindrome ["a"; "t"; "o"; "t"; "a"] in
 
+  print_endline "Problem 01: last [1; 2; 3; 4;]";
   print_problem_one string_of_int problem_one;
+
+  print_endline "Problem 02: last_two [1; 2; 3; 4; 5]";
   print_problem_two string_of_int problem_two;
+
+  print_endline "Problem 03: fd_nth_element ['1'; '44'; '42'; '4'; '100']";
   print_endline problem_three;
+
+  print_endline "Problem 04 and bonus: lst_len ['1'; '2'; '3'; '4']";
   print_endline (string_of_int problem_four);
   print_endline (string_of_int problem_four_tail);
+
+  print_endline "Problem 05: ['1'; '2'; '3'; '4']";
   List.iter print_string problem_five;
-  Printf.printf "\n%b\n" problem_six;;
+
+  print_endline "\nProblem 06: ['a'; 't'; 'o'; 't'; 'a']";
+  Printf.printf "%b\n" problem_six;;
