2623519d3b91ec0b232c9d77d6ebbb7f03fd03a8
Marcos Flavio <mfghost69@gmail.com>
2023-08-16 00:31:07 -0300

    Feat :Solve problem 12.

--- bin/problem_twelve.ml
+++ bin/problem_twelve.ml
@@ -1,5 +1,16 @@
 (* Problem 12 *)
 open Problem_eleven;;
+open Problem_five;;
 
-let decode_rle (_lst: 'a rle list): 'a list =
-  ["A"]
+let rec add n elem lst =
+  if n = 0 then lst
+  else add (n - 1) elem (elem :: lst)
+
+let decode_rle (lst: 'a rle list): 'a list =
+  let rec decode_rle' acc = function
+    | [] -> acc
+    | h :: t ->
+      match h with
+      | One value -> decode_rle' (value :: acc) t
+      | Many (num, elem) -> decode_rle' (add num elem acc) t
+    in rev_lst (decode_rle'[] lst)
