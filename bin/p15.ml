0f234cf72ec14d58fc9b76da74744ff16c12999c
Marcos Flavio <mfghost69@gmail.com>
2023-08-19 13:17:02 -0300

    Feat :Solve problem 15.

--- bin/p15.ml
+++ bin/p15.ml
@@ -1,4 +1,14 @@
 (* Problem 15 *)
+open P05;;
 
-let replicate (_lst: 'a list) (_n): 'a list =
-  ["Hello"]
+let replicate (lst: 'a list) (n: int) =
+  let rec dup n acc elem =
+    match n with
+    | 0 -> acc
+    | _ -> dup (n - 1) (elem :: acc) elem
+  in
+  let rec replicate' n acc = function
+    | [] -> acc
+    | h :: t -> replicate' n (dup n acc h) t
+  in
+  rev_lst (replicate' n [] lst)
