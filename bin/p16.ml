135cfca17b3d192e2d93a65ef51aad29457f1e88
Marcos Flavio <mfghost69@gmail.com>
2023-08-23 20:11:06 -0300

    Feat :Solve problem 16.

--- bin/p16.ml
+++ bin/p16.ml
@@ -1,4 +1,11 @@
 (* Problem 16 *)
+open P05;;
 
-let drop (_lst: 'a list) (_n: int): 'a list =
-  ["a"]
+let drop (lst: 'a list) (n: int): 'a list =
+  let rec drop' acc n n' = function
+    | [] -> acc
+    | [x] when n > 1 -> x :: acc
+    | h :: t ->
+      if n = 1 then drop' acc n' n' t
+      else drop' (h :: acc) (n - 1) n' t
+  in rev_lst (drop' [] n n lst)
