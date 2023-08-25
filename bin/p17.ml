de1b052e1ca6bbe8f3f6b9bf5785af4f9663e503
Marcos Flavio <mfghost69@gmail.com>
2023-08-24 23:37:39 -0300

    Feat :Solve problem 17.

--- bin/p17.ml
+++ bin/p17.ml
@@ -1,7 +1,15 @@
 (* Problem 17 *)
+open P05;;
 
-let split (_lst: 'a list) (_n: int): 'a list * 'a list =
-  (["test"], ["42"])
+let split (lst: 'a list) (n: int): 'a list * 'a list =
+  let rec split' n acc lst =
+    match lst with
+    | [] -> ((rev_lst acc), [])
+    | h :: t ->
+      if n > 0 then split' (n - 1) (h :: acc) t
+      else ((rev_lst acc), h :: t)
+  in
+  split' n [] lst
 
 let print_seventeen (tup: 'a list * 'a list) =
   match tup with
