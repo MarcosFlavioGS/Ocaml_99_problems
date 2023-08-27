39dcd8a4d02f27a5f4ff7a45665b22e48fe2599f
Marcos Flavio <mfghost69@gmail.com>
2023-08-27 15:02:29 -0300

    Refact :Switch lst_len to lst_len_tail in p21.

--- bin/p21.ml
+++ bin/p21.ml
@@ -6,7 +6,7 @@ let insert_at (elem: 'a) n (lst: 'a list): 'a list =
   let rec insert' elem n acc = function
     | h :: t ->
       if n > 0 then insert' elem (n - 1) (h :: acc) t
-      else insert' elem (n + (lst_len t)) (h :: elem :: acc) t
+      else insert' elem (n + (lst_len_tail t)) (h :: elem :: acc) t
     | _ -> acc
   in
   rev_lst (insert' elem n [] lst)
